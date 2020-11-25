41194294bf47977273311cc5f4632ce7db45b6f6
Salvador Hernandez <hernandezg.sal@gmail.com>
2020-11-24 22:49:43 -0800

    Updates

--- popup.js
+++ popup.js
@@ -1,10 +1,9 @@
 var storage = chrome.storage.local;
 
 // When Chrome Extension popup fully loads
-window.addEventListener('load', function (evt) {
+window.addEventListener('load', () => {
   checkInstallType();
   updateUI();
-
   let emojiButton = document.getElementById('checkbox');
   if (emojiButton) {
     emojiButton.addEventListener('change', onSwitchChange);
@@ -12,17 +11,17 @@ window.addEventListener('load', function (evt) {
 });
 
 // When a tab is finished loading
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
   if (changeInfo.status == 'complete') {
-    storage.get('options', function (items) {
+    storage.get('options', (items) => {
       if (items.options.userOptions.replaceWithEmojis) {
         chrome.tabs.executeScript(tabId, {
           file: "replaceText.js"
-        }, function () {
+        }, () => {
           // If you try and inject into an extensions page or the webstore/NTP you'll get an error
           if (chrome.runtime.lastError) {
             // message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
-            console.log('There was an error injecting script : \n' + chrome.runtime.lastError.message)
+            console.log('There was an error injecting script : \n' + chrome.runtime.lastError.message);
           }
         });
       }
@@ -30,7 +29,7 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   }
 })
 
-function onSwitchChange(event) {
+onSwitchChange = (event) => {
   let emojiButton = document.getElementById('checkbox');
   let value = event.target.checked;
   if (emojiButton) {
@@ -39,9 +38,8 @@ function onSwitchChange(event) {
   }
 }
 
-function checkInstallType() {
-  storage.get("freshInstall", function (items) {
-
+checkInstallType = () => {
+  storage.get("freshInstall", (items) => {
     //not a fresh install
     if (items.freshInstall === false) {
 
@@ -58,14 +56,14 @@ function checkInstallType() {
         //set value
         storage.set({
           'options': anObject
-        }, function () {});
+        });
       });
     }
   });
 }
 
 function updateUI() {
-  storage.get('options', function (items) {
+  storage.get('options', (items) => {
     if (items.options) {
       let emojiButton = document.getElementById('checkbox');
       if (emojiButton) {
@@ -82,7 +80,7 @@ function updateUI() {
 function updateData(uType, uValue) {
   let tempOptions = {};
 
-  storage.get('options', function (items) {
+  storage.get('options', (items) => {
     //if the object has been created before
     if (items.options) {
       //store options object
